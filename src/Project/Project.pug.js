551b5dd44e2a3805c14660f75df573416a776ea6
Linzee <d.gmiterko@gmail.com>
2017-02-05 14:06:59 +0100

    Fix blur of ptoject title

--- src/Project/Project.pug.js
+++ src/Project/Project.pug.js
@@ -9,7 +9,7 @@ module.exports = class __Component extends React.Component {
         return React.DOM.article({ className: 'single' + ' ' + 'project' }, React.DOM.div({ className: 'image' }, React.DOM.div({
             'style': { backgroundImage: 'url(' + this.props.project.image + ')' },
             className: 'img-container'
-        }, React.DOM.div({ className: 'row' }, React.DOM.div({ className: 'to-bottom' }, React.DOM.div({ className: 'corners' }, React.DOM.h1(null, this.props.project.title)))))), React.DOM.div({ className: 'row' + ' ' + 'content' + ' ' + 'white-triange-bottom-border' }, React.DOM.div({ className: 'corners' }, __map(this.props.project.entries, (entry, $index) => {
+        }), React.DOM.div({ className: 'row' }, React.DOM.div({ className: 'to-bottom' }, React.DOM.div({ className: 'corners' }, React.DOM.h1(null, this.props.project.title))))), React.DOM.div({ className: 'row' + ' ' + 'content' + ' ' + 'white-triange-bottom-border' }, React.DOM.div({ className: 'corners' }, __map(this.props.project.entries, (entry, $index) => {
             return entry.type === 'text' ? React.createElement(TextEntry, {
                 'key': $index,
                 'entry': entry

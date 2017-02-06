5ab8b06612fc7bfa2535dd5f3e4f7342ee40ee1d
Linzee <d.gmiterko@gmail.com>
2017-02-06 02:23:59 +0100

    Noimage image

--- src/components/PortfolioItem/PortfolioItem.pug.js
+++ src/components/PortfolioItem/PortfolioItem.pug.js
@@ -7,9 +7,9 @@ module.exports = class __Component extends React.Component {
         const {props, state} = this;
         return React.DOM.li({ className: __class('category ' + this.props.project.tags[0]) }, React.createElement(Link, {
             'to': '/project/' + this.props.project.slug,
-            'style': { 'backgroundImage': 'url(' + this.props.project.image + ')' },
+            'style': { 'backgroundImage': 'url(' + (this.props.project.image || require('../../images/noimage.png')) + ')' },
             'target': '#content',
             className: 'slowScrollTo galleryItem'
-        }, React.DOM.img({ 'src': this.props.project.image })), React.DOM.div({ className: 'this' + ' ' + 'props' + ' ' + 'project-meta' }, React.DOM.div({ className: 'meta' }, React.DOM.h2(null, this.props.project.title), React.DOM.p(null, this.props.project.description))));
+        }, React.DOM.img({ 'src': this.props.project.image })), React.DOM.div({ className: 'this' + ' ' + 'props' + ' ' + 'project-meta' + ' ' + __class(!this.props.project.image ? 'noimage' : '') }, React.DOM.div({ className: 'meta' }, React.DOM.h2(null, this.props.project.title), React.DOM.p(null, this.props.project.description))));
     }
 };

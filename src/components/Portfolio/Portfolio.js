581a37520cbb2a9bc04068a17352990b69453d23
Dominik Gmiterko <d.gmiterko@gmail.com>
2017-06-26 17:51:49 +0200

    Silly me, sorting with bool..

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,7 +3,7 @@ import Template from './Portfolio.pug.js';
 import siteSettings from './../../../config/site.json';
 
 class Portfolio extends Component {
-    
+
     render() {
 
         var projects;
@@ -15,7 +15,10 @@ class Portfolio extends Component {
 
         projects = projects.slice();
         projects.sort(function(a, b){
-            return (new Date(a.startedAt).valueOf() < new Date(b.startedAt).valueOf());
+            var t1 = new Date(a.startedAt).valueOf();
+            var t2 = new Date(b.startedAt).valueOf();
+            if(isNaN(t1) ||  isNaN(t2)) return 0;
+            return t2 - t1;
         });
 
         return <Template translator={this.props.route.translator} siteSettings={siteSettings} projects={projects} />;

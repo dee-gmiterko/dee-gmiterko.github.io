b11ca2219a173c6ab81eb148d698bcb43db46da6
Dominik Gmiterko <d.gmiterko@gmail.com>
2017-07-07 00:49:23 +0200

    Add disqus

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,6 @@ class Layout extends Component {
 	render() {
 
 		var loading = this.props.route.projectsService.getProjects().length === 0;
-		console.log(loading, this.props.route.projectsService.getProjects());
 
 		return <Template loading={loading} translator={this.props.route.translator} children={this.props.children} changeLocale={this.props.route.changeLocale} />
 	}
--- src/components/Project/Project.pug.js
+++ src/components/Project/Project.pug.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import __map from 'pug-react-compiler/runtimes/map.js';
 import {Link} from 'react-router';
+import ReactDisqusThread from 'react-disqus-thread';
 import {slowScrollTo} from '../../javascript/utils';
 import Entry from './../projectEntries/Entry/Entry';
 module.exports = class __Component extends React.Component {
@@ -24,6 +25,17 @@ module.exports = class __Component extends React.Component {
         }, this.props.translator('Previous project')), React.createElement(Link, {
             'to': 'project/' + this.props.project.successor.slug,
             className: 'button tiny right'
-        }, this.props.translator('Next project')))))));
+        }, this.props.translator('Next project')))))), React.DOM.div({ className: 'disqus' }, React.DOM.div({ className: 'row' }, React.DOM.h3(null, 'Enjoy comments!'), React.createElement(ReactDisqusThread, {
+            'shortname': 'ienze',
+            'identifier': this.props.project.slug,
+            'title': this.props.project.title,
+            'url': 'http://ienze.me/#/project/' + this.props.project.slug
+        }))), React.DOM.div({ 'id': 'fnav' }, React.DOM.div({ className: 'row' + ' ' + 'text-center' }, React.createElement(Link, {
+            'to': '/portfolio',
+            className: 'button large'
+        }, this.props.translator('More projects')), React.createElement(Link, {
+            'to': '/about',
+            className: 'button large'
+        }, this.props.translator('Contact')))));
     }
 };

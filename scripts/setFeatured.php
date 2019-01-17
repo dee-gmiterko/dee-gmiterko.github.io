db2d2a480b640472b837e632f010a569f058413c
Dominik Gmiterko <d.gmiterko@gmail.com>
2019-01-17 11:26:32 +0100

    Profile 2019

--- scripts/setFeatured.php
+++ scripts/setFeatured.php
@@ -1,6 +1,6 @@
 <?php
 
-$featured = ["girl-in-park", "minecraft-scarecrows", "minecraft-bulldozer", "forest", "ee-antic", "map-london", "icons-extractor", "wordpress-farny", "wordpress-ped", "life-of-snow", "slenderman", "less-sass-color-difference-calculator", "20-4-89-or-50", "efil-fo-emag", "hexaflexagon", "processing-sketches", "processing-ponyo", "trainwreck", "open-gl-forest", "samos", "sheep-simulation", "waterfull", "tower-town-visuals"];
+$featured = ["girl-in-park", "minecraft-scarecrows", "forest", "map-london", "icons-extractor", "wordpress-farny", "wordpress-ped", "life-of-snow", "slenderman", "less-sass-color-difference-calculator", "20-4-89-or-50", "efil-fo-emag", "hexaflexagon", "processing-sketches", "processing-ponyo", "trainwreck", "open-gl-forest", "sheep-simulation", "waterfull", "tower-town-visuals", "tmsei", "kreativator", "py-2049", "percy-s-tower"];
 
 $en = json_decode(file_get_contents("../public/projects/en.json"), true);
 $sk = json_decode(file_get_contents("../public/projects/sk.json"), true);

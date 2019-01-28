d0fd9dd3a657d640db5822d68f163b0689a92285
Dominik Gmiterko <d.gmiterko@gmail.com>
2019-01-28 16:39:56 +0100

    Content, update

--- scripts/setFeatured.php
+++ scripts/setFeatured.php
@@ -1,13 +1,22 @@
 <?php
 
-$featured = ["girl-in-park", "minecraft-scarecrows", "forest", "map-london", "icons-extractor", "wordpress-farny", "wordpress-ped", "life-of-snow", "slenderman", "less-sass-color-difference-calculator", "20-4-89-or-50", "efil-fo-emag", "hexaflexagon", "processing-sketches", "processing-ponyo", "trainwreck", "open-gl-forest", "sheep-simulation", "waterfull", "tower-town-visuals", "tmsei", "kreativator", "py-2049", "percy-s-tower"];
+$featured = ["girl-in-park", "map-london", "ee-antic", "icons-extractor", "20-4-89-or-50", "samos", "efil-fo-emag", "hexaflexagon", "processing-sketches", "processing-ponyo", "trainwreck", "open-gl-forest", "sheep-simulation", "waterfull", "tower-town-visuals", "tmsei", "kreativator", "py-2049", "percy-s-tower", "2d-color-picker"];
 
 $en = json_decode(file_get_contents("../public/projects/en.json"), true);
 $sk = json_decode(file_get_contents("../public/projects/sk.json"), true);
 
 function setFeatured(&$projects, $featured) {
   for($i = 0; $i<count($projects); $i++) {
-    $projects[$i]["featured"] = in_array($projects[$i]['slug'], $featured);
+    $slug = $projects[$i]['slug'];
+    $before = $projects[$i]["featured"];
+    $projects[$i]["featured"] = in_array($slug, $featured);
+    if($projects[$i]["featured"] != $before) {
+      if($before) {
+        echo "$slug UNfeatured \n";
+      } else {
+        echo "$slug featured \n";
+      }
+    }
   }
 }
 
